25f370ef99097e134ef659c2a79fccada54ab63b
matthewcroughan <matt@croughan.sh>
2024-04-17 15:32:35 +0100

    use flake.parts instead of flake-utils

--- flake.nix
+++ flake.nix
@@ -1,24 +1,19 @@
 {
-
-  # nix build -L --builders 'ssh-ng://nix-ssh@100.107.23.115 x86_64-linux,i686-linux - 16 - big-parallel'
-
   description = "A flake for Godot 4 VoIP extension";
-  
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     gdextension-hello-nix = {
       url = "git+https://github.com/goatchurchprime/gdextension-hello-nix?submodules=1";
       flake = false;
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  
-  outputs = { nixpkgs, flake-utils, gdextension-hello-nix, self, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+
+  outputs = inputs@{ flake-parts, gdextension-hello-nix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, ... }: {
         packages.default = pkgs.stdenv.mkDerivation {
           name = "gdextension-hello-nix";
           src = gdextension-hello-nix;
@@ -34,13 +29,14 @@
 
         # To hack the code, do:
         #  nix develop
-        #  scons 
+        #  scons
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             scons
             cmake
          ];
         };
-      });
+      };
+    };
 }
 
