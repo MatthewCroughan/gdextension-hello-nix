998b05ef55a1035934c16dbfc189bac8121fa7f9
matthewcroughan <matt@croughan.sh>
2024-04-17 15:42:30 +0100

    use self and flake inputs instead of vendoring git submodule for godot-cpp

--- flake.nix
+++ flake.nix
@@ -3,20 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    gdextension-hello-nix = {
-      url = "git+https://github.com/goatchurchprime/gdextension-hello-nix?submodules=1";
+    godot-cpp = {
+      url = "github:godotengine/godot-cpp/48afa82f29354668c12cffaf6a2474dabfd395ed";
       flake = false;
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, gdextension-hello-nix, ... }:
+  outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
       perSystem = { pkgs, ... }: {
         packages.default = pkgs.stdenv.mkDerivation {
           name = "gdextension-hello-nix";
-          src = gdextension-hello-nix;
+          src = inputs.self;
+          prePatch = ''
+            cp -r --no-preserve=mode ${inputs.godot-cpp} ./godot-cpp
+          '';
           nativeBuildInputs = [
             pkgs.scons
           ];
