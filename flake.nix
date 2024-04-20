e3211bfa301962242d21eaa9e22f8cd952e0ffaa
matthewcroughan <matt@croughan.sh>
2024-04-20 22:46:18 +0100

    build for android

--- flake.nix
+++ flake.nix
@@ -7,13 +7,28 @@
       url = "github:godotengine/godot-cpp/48afa82f29354668c12cffaf6a2474dabfd395ed";
       flake = false;
     };
+    android.url = "github:tadfisher/android-nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      perSystem = { pkgs, ... }: {
+      perSystem = { pkgs, system, ... }: let
+        androidenv = inputs.android.sdk.x86_64-linux (sdkPkgs: with sdkPkgs; [
+          build-tools-34-0-0
+          cmdline-tools-latest
+          platform-tools
+          platforms-android-34
+          ndk-23-2-8568313
+        ]);
+      in rec {
+        packages.android = packages.default.overrideAttrs {
+          ANDROID_HOME = "${androidenv}/share/android-sdk";
+          sconsFlags = [
+            "platform=android"
+          ];
+        };
         packages.default = pkgs.stdenv.mkDerivation {
           name = "gdextension-hello-nix";
           src = inputs.self;
